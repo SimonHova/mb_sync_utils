3fc1829780c7b9c8e97b5c5c75799d92342ec165
Simon Hova <simon@hova.net>
2022-12-06 12:10:24 -0500

    Brought the code up to date with rating sync. Trying to get it working to pull releases from MB and get rid of old and unused release.

--- mb_collection_sync.py
+++ mb_collection_sync.py
@@ -7,6 +7,9 @@ import sys
 
 import ampache
 
+# Set the default logging level.
+logging.basicConfig(level=logging.INFO)
+
 parser = argparse.ArgumentParser(description='Sync collections between Ampache and MusicBrainz')
 
 parser.add_argument('--config', type=str, help='location of a config file',default=None)
@@ -17,7 +20,7 @@ parser.add_argument('--sync_from',choices=['Ampache','MusicBrainz'], default='Am
 
 parser.add_argument('--MB_ID', type=str, help='The ID used for MusicBrainz')
 parser.add_argument('--MB_PW', type=str, help='The PW used for MusicBrainz')
-parser.add_argument('--MB_Collection', type=str, help='The MBID for your MusicBrainz album collection', default=None)
+parser.add_argument('--MB_collection', type=str, help='The MBID for your MusicBrainz album collection', default=None)
 
 parser.add_argument('--Amp_URL', type=str, help='The URL used for your local Ampache server')
 parser.add_argument('--Amp_API', type=str, help='The API used for your local Ampache server')
@@ -34,8 +37,8 @@ if not args.MB_ID:
     args.MB_ID = config['musicbrainz']['username']
 if not args.MB_PW:
     args.MB_PW = config['musicbrainz']['password']
-if not args.MB_Collection:
-    args.MB_Collection = config['musicbrainz']['collection']
+if not args.MB_collection:
+    args.MB_collection = config['musicbrainz']['collection']
 if not args.Amp_URL:
     args.Amp_URL = config['ampache']['url']
 if not args.Amp_API:
@@ -62,26 +65,39 @@ passphrase = ampacheConnection.encrypt_string(args.Amp_API, args.Amp_ID)
 Amp_key = ampacheConnection.handshake(args.Amp_URL, passphrase)
 
 _offset = 0
+_limit = 5000
 
-while True:
-    # Collect the releases from Ampache
-    amp_results = ampacheConnection.albums(offset=_offset * 5000)
-    
-    if len(amp_results) <= 1: break
+if args.sync_from == 'Ampache':
+    while True:
+        # Collect the releases from Ampache
+        amp_results = ampacheConnection.albums(offset=_offset * _limit)
+        
+        if len(amp_results) <= 1: break
 
-    amp_albums=[]
-    _mbid=None
+        amp_albums=[]
+        _mbid=None
 
-    for album in amp_results:
-        if album.tag == 'album':
-            _mbid   = album.find('mbid').text
-            if _mbid != None:
-                amp_albums.append( _mbid )
-                logging.info("Got " + str(len(amp_albums)) + " albums")
-                
-                _mbid = None
+        for album in amp_results:
+            if album.tag == 'album':
+                _mbid   = album.find('mbid').text
+                if _mbid != None:
+                    amp_albums.append( _mbid )
+                    logging.info("Got " + str(len(amp_albums)) + " albums")
+                    
+                    _mbid = None
 
-    # Push them to MusicBrainz
-    for chunk in [ amp_albums [i:i + 200] for i in range(0, len(amp_albums), 200) ]:
-        musicbrainzngs.add_releases_to_collection(args.MB_Collection, releases=chunk)
-    _offset += 1
+        # Push them to MusicBrainz
+        for chunk in [ amp_albums [i:i + 200] for i in range(0, len(amp_albums), 200) ]:
+            musicbrainzngs.add_releases_to_collection(args.MB_collection, releases=chunk)
+        _offset += 1
+else:
+    while True:
+        # Collect the releases from MusicBrainz
+        mb_results = musicbrainzngs.get_releases_in_collection(args.MB_collection)['collection']['release-list']
+        
+        for release in mb_results['collection']['release-list']:
+            mb_albums = mb_results['id']
+        
+        for mb_album in mb_albums:
+            ampacheConnection.advanced_search(rules=['mbid_album',4,mb_album])
+        
